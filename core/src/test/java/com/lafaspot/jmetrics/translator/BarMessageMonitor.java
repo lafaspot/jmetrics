ba6fd6686774e20a3042ba41cce7fab13220915d
Jason Wang <jpowang@gmail.com>
2016-01-04 17:03:54 -0800

    Version 1.0.3 which contains major refactor in annotation

---
+++ core/src/test/java/com/lafaspot/jmetrics/translator/BarMessageMonitor.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright [yyyy] [name of copyright owner]
+ * 
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  ====================================================================
+ */
+
+package com.lafaspot.jmetrics.translator;
+
+import com.lafaspot.jmetrics.annotation.Alert;
+import com.lafaspot.jmetrics.annotation.Metric;
+import com.lafaspot.jmetrics.annotation.MetricCheck;
+import com.lafaspot.jmetrics.annotation.MetricClass;
+
+/**
+ * Monitor BarMessage for test.
+ *
+ */
+@MetricClass(enable = true, name = "BarMessageMonitor", applications = "test")
+public class BarMessageMonitor {
+
+	/**
+	 * Test latency.
+	 * 
+	 * @return latency
+	 */
+	@Metric(enable = true, type = "latency")
+	@Alert(max = 1000, min = 500)
+	@MetricCheck(type = "count")
+	public long getBarLatency() {
+		return 500;
+	}
+
+	/**
+	 * Test disabled count.
+	 * 
+	 * @return disabled count
+	 */
+	@Metric(enable = false, type = "count")
+	@Alert(max = 2000, min = 90)
+	public long getBarDisabledCount() {
+		return 50;
+	}
+
+	/**
+	 * Test bar count.
+	 * 
+	 * @return bar count
+	 */
+	@Metric(enable = true, type = "count")
+	@Alert(max = 200, min = 100)
+	public long getBarCount() {
+		return 100;
+	}
+}
