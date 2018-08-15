f6bfb831ebfce72bbc964f4770ca9f6c64569da2
Manish Singh <manishsingh@yahoo-inc.com>
2018-08-15 12:08:36 -0700

    fixing the cd scripts for maven repository push

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_2488e4ee5c87_key -iv $encrypted_2488e4ee5c87_iv -in codesigning.asc.enc -out codesigning.asc -d
+if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_fa1ab798fd12_key -iv $encrypted_fa1ab798fd12_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
