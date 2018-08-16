fe6624e1f44e583ac98f93c40504132ab99c4bd7
Manish Singh <manishsingh@yahoo-inc.com>
2018-08-15 17:46:03 -0700

    took changes from kumar

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" == 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn clean deploy -P release-sign-artifacts,build-extras --settings cd/mvnsettings.xml
 fi
